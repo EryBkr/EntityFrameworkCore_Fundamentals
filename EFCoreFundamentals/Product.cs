1c023206594e55661c8d2e8fb1c8434ec027d975
Eray Bakır <info@eraybakir.com>
2020-11-10 01:38:36 +0300

    Created Entities Classes

---
+++ EFCoreFundamentals/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCoreFundamentals
+{
+    public class Product
+    {
+        //Primary Key , Id yazdığımız için birincil anahtar olarak tanımlanır
+        public int Id { get; set; }
+
+        [MaxLength(100)]//Max karakter sayısı 100
+        [Required]//Zorunlu olarak tanımladık
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
