c1983c36586cc06b2bb8fb1ab86246d4fd3c8914
Eray Bakır <info@eraybakir.com>
2020-11-10 16:51:09 +0300

    Context added

---
+++ EFCoreFundamentals/ExampleContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreFundamentals
+{
+    public class ExampleContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Database=ExampleDB;Server=(localdb)\MSSQLLocalDB;Trusted_Connection=True;");//Connection String Eklendi
+        }
+    }
+}
